f586fcac6768196f0ab718723477866b68ebd2d4
Andre Vallestero <andrevallestero@gmail.com>
2021-10-18 09:30:31 -0400

    Add get Nfts, Koii, and Ar by owner

--- src/web.ts
+++ src/web.ts
@@ -1,17 +1,41 @@
-import { Common } from "./common";
+import { Common, arweave } from "./common";
 
 export class Web extends Common {
   /**
-   * Get top contents of user
-   * @param attention_id ID of the attention contract to apply views and attention from
-   * @returns Array of user contents
+   * Get Koii balance
+   * @param address Address to check balance
+   * @returns Balance
    */
-  async myContent(): Promise<Array<unknown> | null> {
-    if (!this.address) return null;
+  async getKoii(address: string): Promise<Number> {
+    this.assertArId(address);
+    const state = await this.getKoiiState();
+    return Object.prototype.hasOwnProperty.call(state.balances, address) ?
+      state.balances[address] : 0;
+  }
+
+  /**
+   * Get Arweave balance
+   * @param address Address to check balance
+   * @returns Balance
+   */
+  async getAr(address: string): Promise<Number> {
+    this.assertArId(address);
+    const winston = await arweave.wallets.getBalance(address);
+    const ar = arweave.ar.winstonToAr(winston);
+    return parseFloat(ar);
+  }
+
+  /**
+   * Get NFT states of address
+   * @param address Address to lookup NFT states
+   * @returns Array of address NFT states
+   */
+  async getNftsByOwner(address: string): Promise<Array<unknown>> {
+    this.assertArId(address);
 
     // Get array of my awaitable NFT states
     const contentViewProms = [];
-    for (const txId of await this.getNftIdsByOwner(this.address))
+    for (const txId of await this.getNftIdsByOwner(address))
       contentViewProms.push(this.getNftState(txId));
 
     // Process NFTs simultaneously then return
@@ -19,6 +43,14 @@ export class Web extends Common {
     return getNftsRes
       .filter((res) => res.status === "fulfilled")
       .map((res: any) => res.value);
+  }
+
+  /**
+   * Get NFT states of loaded address
+   * @returns Array of loaded address NFT states
+   */
+  async myContent(): Promise<Array<unknown>> {
+    return this.getNftsByOwner(this.address as string);
   }
 }
 
