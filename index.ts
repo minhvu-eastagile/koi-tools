6d61a966355d9861206f3df43c9ab68bf023e410
Long Pham <long.pham@eastagile.com>
2022-12-20 10:40:02 +0700

    [c] update package

--- index.ts
+++
@@ -1,2 +0,0 @@
-export * as koi_common from "./src/common";
-export * as koi_web from "./src/web";
--- test/node.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const knode = require("../dist/node");
-const ktools = new knode.Node();
-
-test("Test loading", () => {
-  expect(true).toBe(true);
-})
-
